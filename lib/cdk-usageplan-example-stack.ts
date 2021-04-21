6a1dddf1e1c2e78b620c241927baa0955148c2e0
corymhall <43035978+corymhall@users.noreply.github.com>
2021-04-21 17:31:48 +0000

    fixing

--- lib/cdk-usageplan-example-stack.ts
+++ lib/cdk-usageplan-example-stack.ts
@@ -3,13 +3,10 @@ import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CdkUsageplanExampleStack extends cdk.Construct {
   public readonly usagePlan: apigateway.UsagePlan;
-  public readonly restApi: apigateway.IRestApi;
 
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id);
 
     this.usagePlan = new apigateway.UsagePlan(this, 'MyUsagePlan');
-    
-    this.restApi = apigateway.RestApi.fromRestApiId(this, 'ImportedRestApi', 'dyakuck9a1');
   }
 }
